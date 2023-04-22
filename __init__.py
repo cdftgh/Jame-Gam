c8559945560c28719fbe611dbf68a2e950034c20
Shahar Sarfati <99743592+cdftgh@users.noreply.github.com>
2023-04-22 16:30:02 +0300

    e

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ from game.game import Game
 
 game = Game()
 game.__init__()
-game.handle_events()
+game.run()
--- game/game.py
+++ game/game.py
@@ -8,12 +8,20 @@ class Game:
         self.move_left = False
         self.jump = False
 
-        titlescreen = TitleScreen()
+        self.titlescreen = TitleScreen()
 
-        titlescreen.run_title()
+        self.character = pygame.rect.Rect(50, 20, 50, 20)
+
+        self.screen = pygame.display.set_mode(self.titlescreen.get_screen_size())
+
+        self.bg_pic = self.titlescreen.get_bg_pic()
 
     def handle_events(self):
+        self.screen.blit(self.bg_pic, (0, 0))
+        self.screen.blit(self.character, (self.screen.get_width() // 2, self.screen.get_height() // 2))
+
         for event in pygame.event.get():
+            keys = pygame.key.get_pressed()
             if event.type == pygame.K_SPACE and not self.game_start:
                 self.game_start = True
 
@@ -25,3 +33,20 @@ class Game:
 
             if event.type == pygame.K_SPACE and self.game_start:
                 self.jump = True
+
+            if not keys[pygame.K_RIGHT]:
+                self.move_right = False
+
+            if not keys[pygame.K_LEFT]:
+                self.move_left = False
+
+        if self.move_right:
+            self.character.center += [400]
+
+    def run(self):
+        while True:
+            if self.game_start:
+                self.handle_events()
+
+            elif not self.game_start:
+                self.titlescreen.run_title()

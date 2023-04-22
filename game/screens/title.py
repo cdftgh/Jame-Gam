0af41fbaeaa9103f0e5aed5d41fda66358cfd9d7
Shahar Sarfati <99743592+cdftgh@users.noreply.github.com>
2023-04-22 09:12:47 +0300

    fonts and title screen

--- game/screens/title.py
+++ game/screens/title.py
@@ -4,39 +4,53 @@ from pygame import mixer
 mixer.init()
 pygame.init()
 
-screen = pygame.display.set_mode((804, 680))
-clock = pygame.time.Clock()
+class TitleScreen:
+    screen = pygame.display.set_mode((804, 680))
+    clock = pygame.time.Clock()
 
-bg_pic = pygame.image.load(r'game\images\bg.png')
+    bg_pic = pygame.image.load(r'game\images\bg.png')
 
-fullscreen = False
-clicking_F11 = False
+    fullscreen = False
+    clicking_F11 = False
 
-while True:
-    info = pygame.display.get_window_size()
-    bg_pic = pygame.transform.scale(bg_pic, info)
+    while True:
+        info = pygame.display.get_window_size()
+        bg_pic = pygame.transform.scale(bg_pic, info)
 
-    screen.blit(bg_pic, (0, 0))
+        width, height = info
 
-    for event in pygame.event.get():
-        keys = pygame.key.get_pressed()
+        font = pygame.font.Font(r'game\fonts\Savior2.ttf', 50*(fullscreen+1))
 
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+        title_text = font.render('Leaf Collector', True, (0, 0, 0))
+        title_rect = title_text.get_rect(center=(width // 2, height // 3))
 
-        if keys[pygame.K_F11] and fullscreen == False and clicking_F11 == False:
-            pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-            fullscreen = True
-            clicking_F11 = True
+        font = pygame.font.Font(r'game\fonts\Savior2.ttf', 33*(fullscreen+1))
+        start_text = font.render('Press SPACE to play', True, (0, 0, 0))
+        start_rect = start_text.get_rect(center=(width // 2, height // 3*1.5))
 
-        elif keys[pygame.K_F11] and fullscreen and clicking_F11 == False:
-            pygame.display.set_mode((804, 680))
-            fullscreen = False
-            clicking_F11 = True
+        screen.blit(bg_pic, (0, 0))
+        screen.blit(title_text, title_rect)
+        screen.blit(start_text, start_rect)
 
-        if keys[pygame.K_F11] == False:
-            clicking_F11 = False
+        for event in pygame.event.get():
+            keys = pygame.key.get_pressed()
 
-    pygame.display.update()
-    clock.tick(120)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+            if keys[pygame.K_F11] and fullscreen == False and clicking_F11 == False:
+                pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+                fullscreen = True
+                clicking_F11 = True
+
+            elif keys[pygame.K_F11] and fullscreen and clicking_F11 == False:
+                pygame.display.set_mode((804, 680))
+                fullscreen = False
+                clicking_F11 = True
+
+            if keys[pygame.K_F11] == False:
+                clicking_F11 = False
+
+        pygame.display.update()
+        clock.tick(120)
